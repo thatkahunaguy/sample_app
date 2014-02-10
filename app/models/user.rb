ee59ae1240da19bf2aebe860fecd8a1babfea267
John Glancy <jeglancy@gmail.com>
2014-02-10 11:40:45 -0800

    correct password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   
 # checking for valid email uses a regular expression (regex) to define format
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             # rails infers true for uniqueness since the case_sensitive flag is set
                 uniqueness: { case_sensitive: false }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,7 +47,7 @@ describe User do
     it "should be invalid" do
     # %w makes an array in ruby
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -78,6 +78,17 @@ describe User do
     it { should_not be_valid }
   end
 
+# test that mixed case is saved as lower case
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
 #test for password mismatch
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
@@ -106,5 +117,6 @@ describe "with a password that's too short" do
   before { @user.password = @user.password_confirmation = "a" * 5 }
   it { should be_invalid }
 end
+
 
 end
