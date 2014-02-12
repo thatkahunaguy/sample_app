f1cf9a7a16386074e9f4b6119d31b244985bf4bd
John Glancy <jeglancy@gmail.com>
2014-02-12 11:22:00 -0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,26 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+     # there is no create view so we want to redirect to the user profile just created
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+# defines what parameters are permitted to be passed rather than initializing
+# with the whole params(:user) hash which is a security risk.  A private method
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end  
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,7 +10,35 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-  
+
+# not sure if there is a reason to separate signup from signup page in real life?
+# tutorial did it since the controller & view for create (form submit) not done yet
+  describe "signup" do
+    before { visit signup_path } 
+# user signup testing - useful tests   
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+      # submit with no information/blank, User.count shouldn't change
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
   describe "profile page" do
   # set a user variable using FactoryGirl Gem and spec/factories
     let(:user) { FactoryGirl.create(:user) }
